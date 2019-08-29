b1eb4cf858835abbf6b02845e1234389d0be39e9
Regan Dufort <regan.t.dufort@gmail.com>
2019-08-29 17:38:51 -0400

    initial landing pages and action flow of pages

---
+++ NutraZone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NutraZone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
